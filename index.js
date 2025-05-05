e7a88c0f76c0aae2261927d83f1b00da19c71b31
Abadur Rahman <abadur391@gmail.com>
2025-05-05 10:58:13 +0600

    configed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello From Terminal')
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
+
+// Model
+const model = new ChatGoogleGenerativeAI({
+    model: "gemini-2.0-flash",
+    apiKey: process.env.GOOGLE_API_KEY,
+  });
+
+// Embedding
+const embeddings = new GoogleGenerativeAIEmbeddings({
+    model: "text-embedding-004", // 768 dimensions
+    apiKey: process.env.GOOGLE_API_KEY,
+  });
